d3db09087cbc0c0652484e55873edb68ea3b86d2
Amit Singh <amit.singh@mountblue.tech>
2021-01-31 23:12:29 +0530

    Completed: Create, Read, Update, Delete blog post action

--- src/main/java/com/mountblue/blog/blogapplication/controller/BlogApplicationController.java
+++ src/main/java/com/mountblue/blog/blogapplication/controller/BlogApplicationController.java
@@ -5,9 +5,9 @@ import com.mountblue.blog.blogapplication.service.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Controller
@@ -21,10 +21,62 @@ public class BlogApplicationController {
         this.thePostsService = thePostsService;
     }
 
-    @GetMapping("/posts")
+    // Add mapping for /bloglist
+    @GetMapping("/bloglist")
     public String blogList(Model theModel){
+
         List<Posts> thePostList = thePostsService.findAll();
-        theModel.addAttribute("postList", thePostList);
+        theModel.addAttribute("blogList", thePostList);
         return "blog-list";
     }
+
+    // Add mapping for /showFormForCreateBlog
+    @GetMapping("/showFormForCreateBlog")
+    public String showFormForCreateBlog(Model theModel){
+        Posts thePosts = new Posts();
+        theModel.addAttribute("posts", thePosts);
+        return "create-blog-post";
+    }
+
+    // Add mapping for /showFullBlogPost
+    @GetMapping("/showFullBlogPost")
+    public String showFullBlogPost(@RequestParam("postId") int theId, Model theModel){
+        System.out.println(theId);
+        Posts thePost = thePostsService.findById(theId);
+        theModel.addAttribute("post", thePost);
+        return "full-blog-post";
+    }
+
+    // Add mapping for /showFormForUpdate
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("postId") int theId, Model theModel){
+        Posts thePost = thePostsService.findById(theId);
+        theModel.addAttribute("posts", thePost);
+        return "create-blog-post";
+    }
+
+    // Add mapping for /save
+    @PostMapping("/save")
+    public String saveBlogPost(@ModelAttribute("posts") Posts thePosts){
+        thePosts.setExcerpt(thePosts.getContent());
+        thePosts.setPublishedAt(new Timestamp(System.currentTimeMillis()));
+        thePosts.setPublished(true);
+        thePosts.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        thePosts.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+        thePostsService.save(thePosts);
+        return "redirect:/blog/bloglist";
+    }
+
+    // Add mapping for /delete
+    @GetMapping("/delete")
+    public String delete(@RequestParam("postId") int theId, Model theModel){
+        thePostsService.deleteById(theId);
+        return "redirect:/blog/bloglist";
+    }
 }
+
+
+
+
+
+
